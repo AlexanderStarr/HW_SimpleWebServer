cc494f4b9500e6b9b515bffdadcb582fe97f72fb
Alexander Starr <AlexanderLStarr@gmail.com>
2014-03-05 11:44:35 -0600

    Added updated assignment requirements.  Program now takes in argument and prints it back.

--- server.c
+++ server.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/socket.h>
 
-int main() {
+// Make sure that you check for error codes!  This is best done by using a wrapper
+// function around things (like socket, listen, call, connect).
+// Appropriate actions for any errors are to exit.
+// send and recv should be more intelligent with handling errors.
+
+int main(int argc, char **argv) {
+    int port_no = -1;
+    if (argc != 2) {
+        exit(1);
+    } else {
+        port_no = atoi(argv[1]);
+    }
+    printf("The port number is: %d\n", port_no);
     return 0;
 }
